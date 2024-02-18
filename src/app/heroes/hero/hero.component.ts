1cb68ebfd0b1f22b47945fd9ef050d97c0e04b03
Edgard Buleje <edgard.bulejev@gmail.com>
2024-02-18 14:11:35 -0500

    all changes

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+
+  public name: string = 'ironman';
+  public age: number = 45;
+
+  get capitalizedName():string{
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string{
+    return `${this.name} - ${this.age}`
+  }
+
+  changeHero(nameNew:string):void{
+    this.name = nameNew;
+  }
+
+  changeAge(ageNew:number):void{
+    this.age = ageNew;
+  }
+
+}
